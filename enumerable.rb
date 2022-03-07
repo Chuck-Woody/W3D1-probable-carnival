a1bf953ccfaa3a63da708df0c1f6e13c0461dc80
Matia Kim <matia.h.kim@gmail.com>
2022-03-07 15:46:13 -0800

    up to my_join

--- enumerable.rb
+++ enumerable.rb
@@ -58,11 +58,7 @@ class Array
             if ele.instance_of?(Array)
                arr += ele.my_flatten 
             else
-                puts ele
-                puts "-------"
-                arr << ele  
-                puts "array -------"  
-                puts arr      
+                arr << ele     
             end
 
         end
@@ -71,5 +67,40 @@ class Array
 
     end
 
+    def my_zip(*ele)
+        arr = []
+        i = 0
+        while i < self.length
+            sub_arr = []
+            sub_arr << self[i]
+            ele.my_each do |nums|
+                sub_arr << nums[i]
+            end
+            arr << sub_arr
+            i += 1
+        end
+        arr
+    end
+
+    def my_rotate(n = nil)
+        arr = self.dup
+        if n == nil
+            arr << arr.shift
+        elsif n < 0
+            n *= -1
+            n.times {arr.unshift(arr.pop)}
+        else 
+            n.times {arr << arr.shift}
+        end
+        arr
+    end
+
 end
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
